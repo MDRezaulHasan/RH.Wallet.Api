59602fbe244d296cf23e2a4cdf44ad98736c9aec
Rezaul <rezaulhasan0168@gmail.com>
2025-06-09 20:41:06 +0200

    cron file added

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 import { initDB } from './config/db.js';
 import rateLimiter from './middleware/rateLimiter.js';
 import transactionsRoute from './routes/transactionsRoute.js';
+import job from './config/cron.js'; // Import the job if needed
+import { stat } from 'fs';
 
 dotenv.config();
 
@@ -10,6 +12,14 @@ const PORT = process.env.PORT || 5001;
 
 const app = express();
 
+
+if(process.env.NODE_ENV === 'production') {
+  job.start(); // Start the cron job if in production environment
+}
+
+app.get("/api/health",(req,res)=>{
+res.status(200).json({status: "OK", message: "API is running smoothly!"});
+})
 // Middlewares
 app.use(rateLimiter); // Apply rate limiting middleware
 app.use(express.json()); // Middleware to parse JSON bodies
